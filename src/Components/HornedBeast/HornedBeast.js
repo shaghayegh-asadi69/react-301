5a9b00b186168a198cc07e5e893d897dce66c004
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-07-17 20:11:16 +0100

    update

--- src/Components/HornedBeast/HornedBeast.js
+++ src/Components/HornedBeast/HornedBeast.js
@@ -3,7 +3,7 @@ export default function HornedBeast(props) {
   return (
     <div>
       <h1>{props.title}</h1>
-      <img src={props.url} alt={props.description} />
+      <img src={props.imgUrl} alt={props.description} />
       <p>{props.description}</p>
     </div>
   );
