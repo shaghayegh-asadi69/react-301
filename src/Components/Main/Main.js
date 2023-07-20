fd97c1f4edf16cddc7eaf179d443c0dea1610fe6
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-07-20 08:43:41 +0100

    update

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,22 @@
-import HornedBeast from "../HornedBeast/HornedBeast";
-import data from "../../data.json"
-import "./Main.css"
+import React from "react";
+import "./Main.css";
 
-export default function Main() {
+import HornedBeast from "../HornedBeast/HornedBeast";
+
+export default function Main({ data, handleModal }) {
   return (
-    <div className="main-div">
-     <HornedBeast title={data[0].title} imgUrl={data[0].image_url} description={data[0].description} />
+    <main>
+      {/* <HornedBeast
+        title="Uniwhal"
+        imgUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+        description="A unicorn and a narwhal nuzzling their horns"
+      />
+      <HornedBeast
+        title="Rhino Family"
+        imgUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+        description="Parent rhino with two babies"
+      /> */}
+      {/* <HornedBeast title={data[0].title} imgUrl={data[0].image_url} description={data[0].description} />
       <HornedBeast title={data[1].title} imgUrl={data[1].image_url} description={data[1].description} />
       <HornedBeast title={data[2].title} imgUrl={data[2].image_url} description={data[2].description} />
       <HornedBeast title={data[3].title} imgUrl={data[3].image_url} description={data[3].description} />
@@ -24,7 +35,19 @@ export default function Main() {
       <HornedBeast title={data[16].title} imgUrl={data[16].image_url} description={data[16].description} />
       <HornedBeast title={data[17].title} imgUrl={data[17].image_url} description={data[17].description} />
       <HornedBeast title={data[18].title} imgUrl={data[18].image_url} description={data[18].description} />
-      <HornedBeast title={data[19].title} imgUrl={data[19].image_url} description={data[19].description} />
-    </div>
+      <HornedBeast title={data[19].title} imgUrl={data[19].image_url} description={data[19].description} /> */}
+      {data.map((beast, key) => {
+        return (
+          <HornedBeast
+            key={beast._id}
+            beastObject={beast}
+            title={beast.title}
+            imgUrl={beast.image_url}
+            description={beast.description}
+            handleModal={handleModal}
+          />
+        );
+      })}
+    </main>
   );
 }
